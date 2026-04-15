2d0242b7d021ef8aed5465a495ac9850839fd437
SaiPranav25 <bsaipranav_cse2205d5@mgit.ac.in>
2026-04-15 18:36:59 +0530

    LED fade using PWM

---
+++ 12_led_fade_PWM.ino
@@ -0,0 +1,18 @@
+int led=10;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i=0;i<=255;i++){
+    analogWrite(led,i);
+    delay(10);
+  }
+  
+for(int i=255;i>=0;i--){
+    analogWrite(led,i);
+    delay(10);
+  }
+}
